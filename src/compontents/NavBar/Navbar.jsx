d574205205a69ff094335714fa1667062592ea09
Samuel Rybar <samuel.rybar@cargo-partner.com>
2023-03-15 16:06:45 +0100

    navbar progress

--- src/compontents/NavBar/Navbar.jsx
+++ src/compontents/NavBar/Navbar.jsx
@@ -4,9 +4,17 @@ export const Navbar = () => {
     return (
         <div className='navBar flex justify-between items-center p-[3rem]'>
             <div className='logoDiv'>
-                <h1 className='logo text-[25px] text-blueColor'>
+                <h1 className='logo text-[25px] text-greenColor'>
                     <strong>Vite</strong>Jobs
                 </h1>
+            </div>
+            <div className='menu flex gap-8'>
+                <li className="menuList text-textColor hover:text-greenColor">Jobs </li>
+                <li className="menuList text-textColor hover:text-greenColor">Companies </li>
+                <li className="menuList text-textColor hover:text-greenColor">About </li>
+                <li className="menuList text-textColor hover:text-greenColor">Contact</li>
+                <li className="menuList text-textColor hover:text-greenColor">Register </li>
+                <li className="menuList text-textColor hover:text-greenColor">Login </li>
             </div>
         </div>
     )
