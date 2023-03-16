47b23277a304e6c76d3ce3cd4cec5a4bd8c249d8
Samuel Rybar <samuel.rybar@cargo-partner.com>
2023-03-16 10:38:35 +0100

    updateted search bar with dropdowns

--- src/compontents/SearchDiv/Search.jsx
+++ src/compontents/SearchDiv/Search.jsx
@@ -1,18 +1,61 @@
 import React from 'react'
 import { GoSearch } from 'react-icons/go';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 export const Search = () => {
   return (
-    <div className='searchDiv grid gp-10 bg-darkerGrey rounded-[10px] p-[3rem]'>
+    <div className='searchDiv grid gap-5 bg-darkerGrey rounded-[10px] p-[3rem]'>
       <form action=''>
         <div className='firstDiv flex  justify-between items-center 
         rounded-[8px] gap-[10px] bg-white p-5  shadow-black-500/50 shadow-lg'>
 
-          <div className='flex gap-2 items-center w-[100%]'>
+          <div className='flex gap-1 items-center w-[100%]'>
             <GoSearch className='icon text-[25px]' />
             <input type='text' className='bg-transparent text-green-800 focus:outline-none w-[100%]' placeholder='Search your job here...'></input>
+            <AiOutlineCloseCircle className='text-[30px] text-greyColor hover:text-darkerGrey icon' />     
           </div>
+          <div className='flex gap-1 items-center w-[100%]'>
+            <GoSearch className='icon text-[25px]' />
+            <input type='text' className='bg-transparent text-green-800 focus:outline-none w-[100%]' placeholder='Location'></input>
+            <AiOutlineCloseCircle className='text-[30px] text-greyColor hover:text-darkerGrey icon' />     
+          </div>
+          <button className='bg-greenColor h-full p-5 px-10 rounded-[10px] text-black hover:shadow-black-500/50 hover:shadow-lg'>search</button>          
         </div>
       </form>
+
+      <div className='secDiv flex items-center gap-5 justify-center'>
+      <div className='single Search flex items-center gap-2'>
+          <label htmlFor='relevance' className='text-darkGrey font-semibold'>
+            Sort by:
+            <select name='' id='relevance' className='bg-whiteSmoke rounded-[3px] px-4 py-1'>
+            <option value="">Relevance</option>
+            <option value="">Start With</option>
+            <option value="">Contains</option>
+            </select>
+          </label>
+        </div>
+        <div className='single Search flex items-center gap-2'>
+          <label htmlFor='relevance' className='text-darkGrey font-semibold'>
+            Sort by:
+            <select name='' id='relevance' className='bg-whiteSmoke rounded-[3px] px-4 py-1'>
+            <option value="">Relevance</option>
+            <option value="">Start With</option>
+            <option value="">Contains</option>
+            </select>
+          </label>
+        </div>
+        <div className='single Search flex items-center gap-2'>
+          <label htmlFor='relevance' className='text-darkGrey font-semibold'>
+            Sort by:
+            <select name='' id='relevance' className='bg-whiteSmoke rounded-[3px] px-4 py-1'>
+            <option value="">Relevance</option>
+            <option value="">Start With</option>
+            <option value="">Contains</option>
+            </select>
+          </label>
+        </div>
+
+      </div>
+      
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'greenColor': '#099c11',
-        'cardShadow': '#f0f1f2',
-        'textColor': '#2e302f',
-        'greyColor': '#dee0e3',
-        'whiteSmoke': '#f5f5f5',
-        'darkerGrey': '#b5b5b5'
-      }
+        greenColor: "#099c11",
+        cardShadow: "#f0f1f2",
+        textColor: "#2e302f",
+        greyColor: "#dee0e3",
+        whiteSmoke: "#f5f5f5",
+        darkerGrey: "#b5b5b5",
+        darkGrey: "#4a4a4a",
+      },
     },
   },
   plugins: [],
-}
+};
